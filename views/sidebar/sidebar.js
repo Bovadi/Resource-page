be481f029755330afca1274308995122741a1c48
Bovadi <Bovadi@gmail.com>
2026-02-13 16:41:50 -0800

    Updated sidebar.js

--- views/sidebar/sidebar.js
+++ views/sidebar/sidebar.js
@@ -71,7 +71,7 @@ export class Sidebar {
     if (!filtersContainer) return;
 
     const renderCheckbox = (filterName, label, isChecked) => `
-      <label class="flex items-center gap-3 w-full py-3 px-4 rounded-lg cursor-pointer hover:bg-[#000000] transition-all duration-200 select-none">
+      <label class="flex items-center gap-3 w-full py-3 px-4 rounded-lg cursor-pointer hover:bg-[#F0F3F3] transition-all duration-200 select-none">
         <input
           type="checkbox"
           id="filter-${filterName}"
