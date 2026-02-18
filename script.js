8c5a974c94416203c1b3266e77eb2f53b09eccc4
Bovadi <Bovadi@gmail.com>
2026-02-18 14:15:43 -0800

    Updated style.css

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ import { Sidebar } from './views/sidebar/sidebar.js';
 import { CardGrid } from './views/card-grid/card-grid.js';
 import { Modal } from './views/modal/modal.js';
 import { SAMPLE_CARDS } from './src/data/demoData.js';
-import { initTooltips } from './src/lib/tooltip.js';
 
 class App {
   constructor() {
@@ -30,7 +29,6 @@ class App {
       await this.loadComponents();
       this.setupEventHandlers();
       this.filterAndDisplayCards();
-      initTooltips();
     } catch (err) {
       console.error('App failed to initialize:', err);
       const root = document.getElementById('main-container');
